24a89ab3db3b73957944c2bf5f340ac397bc7abe
Hairy Blue <pecision.nickimarty@gmail.com>
2023-12-10 00:43:53 +0800

    change document title and adding about page

--- views/index.view.php
+++ views/index.view.php
@@ -1,9 +1,7 @@
 <?php require "partials/head.php" ?>
 
 <main class="w-full h-screen">
-
     <div class="bg-white">
-
         <div class="relative isolate px-6 lg:px-8">
             <div class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
                 <div class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
@@ -19,7 +17,7 @@
                     <p class="mt-6 text-lg leading-8 text-gray-600">The e-clearance application simplifies and accelerates student clearance by providing a digital platform for streamlined submission and verification of academic, financial, and administrative requirements.</p>
                     <div class="mt-10 flex items-center justify-center gap-x-6">
                         <a href="/login" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Get started</a>
-                        <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Learn more <span aria-hidden="true">→</span></a>
+                        <a href="/about" class="text-sm font-semibold leading-6 text-gray-900">Learn more <span aria-hidden="true">→</span></a>
                     </div>
                 </div>
             </div>
